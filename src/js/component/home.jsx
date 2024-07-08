00019b29aa094c2871bee2108b272e93d7ce70ec
Letimachado <leti.machado.gerez@gmail.com>
2024-07-08 21:06:52 +0000

    Casi

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,45 @@
-import React from "react";
+import React, {useState} from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
+
+//let nuevoTodo = "";
+const [nuevoTodo, setNuevoTodo] = useState("")
+
+const [todos, setTodos] = useState([""]);
+
+const handleClick = () => {
+	console.log(nuevoTodo)
+	setTodos([...todos, nuevoTodo])
+}
+
+const deleteTodo = (indice) => {
+	const listaNueva = todos.filter((todo, i) => i !== indice)
+	setTodos(listaNueva);
+}
+
+const handleChange = (event) => {
+	setNuevoTodo(event.target.value);
+}
+
+
 	return (
 		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<h1 className="text-center mt-5">To do list</h1>
+			<div className="input-group mb-3" >
+  				<input type="text" class="form-control" aria-label="To do" aria-describedby="button-addon2" onChange={handleChange} />
+  				<button onClick={handleClick} className="btn btn-light" type="button" id="button-addon2">Button</button>
+			</div>
+				<ul className="list-group">
+				{todos.map((todo, indice) => {
+					return(
+						<li className="list-group-item">
+							{todo} <button onClick={() => deleteTodo(indice)} type="button" className="btn-close" aria-label="Close"></button>
+						</li>
+					)
+				})}
+			</ul>
 		</div>
+
 	);
 };
 
